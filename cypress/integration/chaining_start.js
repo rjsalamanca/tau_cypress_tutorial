9b0da47f697d2184273ad92e5e8c9c984aed71bc
rjsalamanca <salamancaronito@gmail.com>
2022-02-17 10:38:48 -0500

    chapter 3

--- cypress/integration/chaining_start.js
+++ cypress/integration/chaining_start.js
@@ -8,5 +8,17 @@ beforeEach(() => {
 
 it('Chaining commands', () => {
 
-   cy.contains('milk')
+   // cy.contains('milk')
+
+   cy.get('[data-cy=task]')
+      .should(item => {
+         if (item.length !== 3) {
+            throw new Error('Not enough elements');
+         }
+         expect(item[0]).to.contain.text('bread');
+         expect(item[1]).to.contain.text('milk');
+      })
+
+   //then is not retired, should is retried
+   //errors will stop continuation of cypress tests
 });
