f6d0b42985baffdf2da53a9b50752ac7198394be
rjsalamanca <salamancaronito@gmail.com>
2022-02-17 15:29:36 -0500

    chapter 8

--- cypress/integration/stubbing_response_start.js
+++ cypress/integration/stubbing_response_start.js
@@ -2,13 +2,20 @@
 
 it('Stubbing response', () => {
 
-  cy
-    .intercept({
-      method: 'GET',
-      url: '/api/boards'
-    }).as('boardList')
+   cy
+      .intercept({
+         method: 'POST',
+         url: '/api/boards'
+      }, {
+         forceNetworkError: true
+      }).as('createdBoard')
 
-  cy
-    .visit('/')
+   cy
+      .visit('/')
 
+   cy.get('[data-cy=create-board]').click();
+
+   cy.get('[data-cy=new-board-input]').type('new board{enter}')
+
+   cy.get('#errorMessage').should('be.visible');
 });
