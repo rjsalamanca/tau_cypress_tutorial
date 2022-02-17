dffe3c2a762f57a7ac548e5f8e8c4e0eaa6c3b36
rjsalamanca <salamancaronito@gmail.com>
2022-02-17 15:09:19 -0500

    chapter 6

--- cypress/integration/sending_requests_start.js
+++ cypress/integration/sending_requests_start.js
@@ -2,7 +2,22 @@
 
 it('Sending requests', () => {
 
-  cy
-    .visit('/')
+   cy
+      .visit('/')
 
+   // cy.request({
+   //    method: 'POST',
+   //    url: '/api/boards',
+   //    body: {
+   //       name: 'board created by .request() command'
+   //    }
+   // })
+
+   cy.request({
+      method: 'PATCH',
+      url: '/api/boards/36722706109',
+      body: {
+         name: 'board created by .request() Patch command'
+      }
+   })
 });
